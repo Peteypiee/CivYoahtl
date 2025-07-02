79fe8cb92b0553c189522bc60cdee023d6fb74ad
Peteypiee <39573595+Peteypiee@users.noreply.github.com>
2025-07-02 12:47:34 -0400

    Update OneFarm.js
    Added announcement system and farm regrow times, added the vine farm, and added a temporary melon seed planting script

--- OneFarm.js
+++ OneFarm.js
@@ -1,13 +1,16 @@
 /*
-BoatCat's OneFarm system allows you to set one farm macro, one time. V1.0.0, 6/24/25
+BoatCat's OneFarm system allows you to set one farm macro, one time. V1.1.0, 7/2/25
 No more having to constantly switch your farm key, and no more keyboard clutter from having multiple farm keybinds!
 Fill in your own farms. Use the format provided by the example Yoahtl farms, with as precise corner/boundary values as possible
 If you would like to have farms added to the default script, please let me know via Discord, @Peteypiee
 */
 
+// VARIABLES
+const p = Player.getPlayer();
+
 // CLASSES
 class farm {
-	constructor(xEast, xWest, zNorth, zSouth, material="", woodFarm=false) {
+	constructor(xEast, xWest, zNorth, zSouth, material="", regrowTime = "Unknown", woodFarm=false) {
 		this.xEast = xEast;
 		this.xWest = xWest;
 		this.zNorth = zNorth;
@@ -15,9 +18,10 @@ class farm {
 		this.material = material;
 		this.woodFarm = woodFarm;
 		this.path = material + ".js";
+		this.regrowTime = regrowTime;
 	}
 	
-	setScriptPath(path) {
+	setScript(path) {
 		this.path = path;
 	}
 	
@@ -32,7 +36,7 @@ class farm {
 		}
 	}
 	
-	getItems() { // TO IMPLEMENT
+	getItems() { // UNUSED
 		let items = [];
 		if (woodFarm) {
 			items.push("minecraft:" + this.material + "_sapling");
@@ -45,26 +49,47 @@ class farm {
 		}
 		return items;
 	}
+	
+	announce(group) {
+		Chat.say("/g " + group);
+		Client.waitTick(2);
+		let now = new Date();
+		Chat.say(group + "==" + this.material + " farm started: " + now.toLocaleString() + ".  Estimated time to regrow: " + this.regrowTime);
+		Client.waitTick(2);
+		Chat.say("/local");
+	}
 }
 
-// VARIABLES
-const p = Player.getPlayer();
-
+// FARMS
 var farms = [];
-const wheatFarm = new farm(6384, 6225, -1426, -1303, material="wheat");
+
+const wheatFarm = new farm(6384, 6225, -1426, -1303, material="wheat", regrowTime = "8 h");
 farms.push(wheatFarm);
-const carrotFarm = new farm(6607, 6416, -1520, -1377, material="carrot"); // NOT YET PORTED TO GITHUB
+
+const carrotFarm = new farm(6607, 6416, -1520, -1377, material="carrot", regrowTime = "16 h"); // NOT ON GITHUB
 farms.push(carrotFarm);
-const potatoFarm = new farm(3624, 3465, 8274, 8397, material="potato");
+
+const potatoFarm = new farm(3624, 3465, 8274, 8397, material="potato"); // MISSING REGROW TIME. FILL IN ON NEXT TRIP
 farms.push(potatoFarm);
-const oakFarm = new farm(6398, 6258, -2016, -1861, material="oak", woodFarm=true); // NOT YET PORTED TO GITHUB
+
+const oakFarm = new farm(6398, 6258, -2016, -1861, material="oak", regrowTime = "12 h", woodFarm=true); // NOT ON GITHUB
 farms.push(oakFarm);
-const jungleFarm = new farm(7581, 7441, -110, 75, material="jungle", woodFarm=true); // NOT YET PORTED TO GITHUB
-jungleFarm.setScriptPath("YoahtlJungle.js");
+
+const jungleFarm = new farm(7581, 7441, -110, 75, material="jungle", regrowTime = "15 h", woodFarm=true); // NOT ON GITHUB
+jungleFarm.setScript("YoahtlJungle.js");
 farms.push(jungleFarm);
-const sugarFarm = new farm(7726, 7632, -192, -97, material="sugar_cane"); // NOT YET PORTED TO GITHUB
-sugarFarm.setScriptPath("sugar.js"); // NOTE: sugar_cane vs. sugar.js
+
+const sugarFarm = new farm(7726, 7632, -192, -97, material="sugar_cane", regrowTime = "12 h"); // NOT ON GITHUB
+sugarFarm.setScript("sugar.js"); // NOTE: sugar_cane vs. sugar.js
 farms.push(sugarFarm);
+
+const vineFarm = new farm(6320, 6239, -1390, -1348, material="vines");
+//farms.push(vineFarm); // COORDINATES OVERLAP WITH CENTER OF WHEAT FARM, UNCOMMENT IF YOU RUN THIS FARM
+
+// TEMP MELON FARM PLANTING SCRIPT
+const melonPlace = new farm(8430, 8322, -1484, -1383, material="melon");
+melonPlace.setScript("melonPlace.js");
+farms.push(melonPlace);
 
 // FUNCTIONS
 function getCurrentFarm() {
@@ -74,11 +99,12 @@ function getCurrentFarm() {
 			currentFarm = farms[i];
 		}
 	}
-	
 	if (currentFarm == null) {
 		throw "Not in a recognized farm. Find a farm or update your farm list";
 	}
-	Chat.log("Running " + currentFarm.material + " farm");
+	
+	let group = "hue";
+	currentFarm.announce(group);
 	JsMacros.runScript(currentFarm.path);
 }
 
