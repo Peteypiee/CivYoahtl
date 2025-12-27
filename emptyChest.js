c17f80b7d2db60a3f21b8b62419388a30817c5de
Peteypiee <39573595+Peteypiee@users.noreply.github.com>
2025-12-27 16:53:06 -0500

    Update emptyChest.js
    Added functionality of emptying Droppers/Hoppers

--- emptyChest.js
+++ emptyChest.js
@@ -8,55 +8,57 @@ var farmItems = [];
 var slots = [];
 
 function emptyChest(){
-	p.interact();
-	Client.waitTick(4);
-	inv = Player.openInventory();
-	Client.waitTick(4);
-	
-	let slotCount = inv.getTotalSlots();
-	let chestSlots = 0;
-	//Chat.log(slotCount);
-	if (slotCount == 46) {
-		throw "Please open a chest";
-	} else if (slotCount == 90) {
-		chestSlots = 54; // Double Chest
-	} else if (slotCount == 41) {
-		chestSlots = 4; // Hopper
-	} else if (slotCount == 63) {
-		chestSlots = 27; // Barrel/Single Chest
-	}
-	
-	for (var slot=0; slot<54; slot++) {
-		let itemId = inv.getSlot(slot).getItemID()
-		let empty = (itemId == `minecraft:air`);
-		
-		if (!empty) {
-			if (farmMode) {
-				if (!farmItems.includes(itemId)) {
-					farmItems.push(itemId);
-					//Chat.log("Pushing " + slot + ": " + itemId + " " + empty);
-					slots.push(slot);
-				}
-			} else { // Not empty slot, not in a farm: move it
-				slots.push(slot)
-				Client.waitTick();
-			}
-		}
-	}
+    p.interact();
+    Client.waitTick(4);
+    inv = Player.openInventory();
+    Client.waitTick(4);
     
-	//Chat.log(slots)
-	for (let i = 0; i < slots.length; i++) {
-		//Chat.log(slots[i]);
-		
-		if (farmMode) {
-			inv.quickAll(slots[i]);
-		} else {
-			inv.quick(slots[i]);
-		}
-		Client.waitTick(1);
-	}
-	
-	inv.close()
+    let slotCount = inv.getTotalSlots();
+    let chestSlots = 0;
+    //Chat.log(slotCount);
+    if (slotCount == 46) {
+        throw "Please open a chest";
+    } else if (slotCount == 90) {
+        chestSlots = 54; // Double Chest
+    } else if (slotCount == 41) {
+        chestSlots = 4;  // Hopper
+    } else if (slotCount == 63) {
+        chestSlots = 27; // Barrel/Single Chest
+    } else if (slotCount == 45) {
+        chestSlots = 9;  // Dropper/Hopper
+    }
+    
+    for (var slot=0; slot<chestSlots; slot++) {
+        let itemId = inv.getSlot(slot).getItemID();
+        let empty = (itemId == `minecraft:air`);
+        
+        if (!empty) {
+            if (farmMode) {
+                if (!farmItems.includes(itemId)) {
+                    farmItems.push(itemId);
+                    //Chat.log("Pushing " + slot + ": " + itemId + " " + empty);
+                    slots.push(slot);
+                }
+            } else { // Not empty slot, not in a farm: move it
+                slots.push(slot);
+                Client.waitTick();
+            }
+        }
+    }
+    
+    //Chat.log(slots)
+    for (let i = 0; i < slots.length; i++) {
+        //Chat.log(slots[i]);
+        
+        if (farmMode) {
+            inv.quickAll(slots[i]);
+        } else {
+            inv.quick(slots[i]);
+        }
+        Client.waitTick(1);
+    }
+    
+    inv.close();
 }
 
 emptyChest();
